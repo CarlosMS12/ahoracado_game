84c84397a6d21a27b804b19a8b792b9893582408
CarlosMS12 <carlosmagallanes207@gmail.com>
2023-12-27 16:38:42 -0500

    Show correct letters in Win/Loss message

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {letters} from './helpers/letters';
 import {HangImage} from './components/HangImage';
 import './App.css';
@@ -7,8 +7,29 @@ function App() {
 	const [word] = useState('COMPUTADORA');
 	const [hiddenWord, setHiddenWord] = useState('_ '.repeat(word.length));
 	const [attempts, setAttempts] = useState(0);
+	const [lose, setLose] = useState(false);
+	const [won, setWon] = useState(false);
+
+
+	// Determinar si la persona ha perdido
+	useEffect(() => {
+		if (attempts >= 9) {
+			setLose(true);
+		}
+	});
+
+	// Determinar si la persona ha ganado
+	useEffect(() => {
+		const currentHiddenWord = hiddenWord.split(' ').join('');
+		if (currentHiddenWord === word) {
+			setWon(true);
+		}
+	})
 
 	const checkLetter = (letter: string) => {
+		if (lose) return;
+		if (won) return;
+
 		if (!word.includes(letter)) {
 			setAttempts(Math.min(attempts + 1, 9));
 			return;
@@ -17,23 +38,32 @@ function App() {
 		const hiddenWordArray = hiddenWord.split(' ');
 
 		for (let i = 0; i < word.length; i++) {
-			if ( word[i] === letter) {
+			if (word[i] === letter) {
 				hiddenWordArray[i] = letter;
 			}
 		}
 		setHiddenWord(hiddenWordArray.join(' '));
+
+		
 	};
 	return (
 		<div className="App">
 			{/* Imégenes */}
 			<HangImage imageNumber={attempts} />
-
 			{/* Palabra oculta */}
 			<h3>{hiddenWord}</h3>
-
 			{/* Contador de intentos */}
 			<h3>Intentos: {attempts}</h3>
+			{/* Mensaje de derrota */}
+			{
+				(lose) ? <h2>Perdiste <br /> La palabra correcta era: {word}</h2> : ''
 
+			}
+			{/* Mensaje de victoria */}
+			{
+				(won) ? <h2>Felicidades, Ganaste</h2> : ''
+
+			}
 			{/* Botones de letras */}
 			{letters.map((letter) => (
 				<button onClick={() => checkLetter(letter)} key={letter}>
