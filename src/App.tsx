6a79a1a685cf21faa6afa0bf5e464a5c26b698e2
CarlosMS12 <carlosmagallanes207@gmail.com>
2023-12-27 15:37:31 -0500

    attempts & setAttempts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
+import {useState} from 'react';
 import {letters} from './helpers/letters';
-import { HangImage } from './components/HangImage';
+import {HangImage} from './components/HangImage';
 import './App.css';
 
 function App() {
+	const [attempts, setAttempts] = useState(0);
+
+	const checkLetter = (letter: string) => {
+		console.log(letter);
+		setAttempts(Math.min(attempts + 1, 9));
+	};
 	return (
 		<div className="App">
 			{/* Imégenes */}
-			<HangImage imageNumber={9}/>
+			<HangImage imageNumber={attempts} />
 
 			{/* Palabra oculta */}
 			<h3>_ _ _ _ _ _ _ _ _ _</h3>
 
 			{/* Contador de intentos */}
-			<h3>Intentos: 0</h3>
+			<h3>Intentos: {attempts}</h3>
 
 			{/* Botones de letras */}
 			{letters.map((letter) => (
-				<button 
-          key={letter}>
-            {letter}
-        </button>
+				<button onClick={() => checkLetter(letter)} key={letter}>
+					{letter}
+				</button>
 			))}
 		</div>
 	);
